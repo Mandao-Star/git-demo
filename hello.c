cecba42d2a7646bd0af1991cb8e532e4368b869e
woqu <woqu@atworld.com>
2025-02-22 09:51:14 +0800

    second commit

--- hello.c
+++ hello.c
@@ -5,23 +5,4 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
+&&&&&&&&&&&&&&&&&
